fe6c47a258779c2cbd40d834adde83bc7b76a8fd
JasfferTP <69882335+Jasffertp@users.noreply.github.com>
2020-08-27 23:34:05 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>JUST world</title>
+<script src="https://kit.fontawesome.com/3b23dca747.js" crossorigin="anonymous"></script>
+<style>
+	@font-face{
+		font-family: roboto-light;
+		src: url('font/Roboto-Light.ttf');
+	}
+	@font-face{
+		font-family: oswald-medium;
+		src: url('font/Oswald-Light.ttf');
+	}
+	@font-face{
+		font-family: baloo;
+		src: url('font/BalooTamma2-Regular.ttf');
+	}
+	body{
+		background-color: #4682B4 ;
+		margin: 0px;
+	}
+	.profile{
+		height: 500px;
+		width: 900px;
+		position: relative;
+		background-color: white;
+		margin-left: auto;
+		margin-right: auto;
+		margin-top: 90px;
+		padding-right: 50px;
+	}
+	.image{
+		width: 335px;
+		height: 500px;
+		float: left;
+		margin-right: 30px;
+		position: relative;
+	}
+	.name{
+		font-family: oswald-medium;
+		font-size: 70px;
+		padding-top: 60px;
+		margin-bottom: 0px;
+	}
+	.section{
+		float: left;
+		font-family: baloo;
+		font-size: 16px;
+		margin: 0px;
+		padding-right: 20px;
+		color: #b3b6b7;
+	}
+	.contact{
+		font-family: baloo;
+		font-size: 16px;
+		margin: 0px;
+		color: #b3b6b7;
+	}
+	.line{
+		width: 300px;
+		height: 1px;
+		float: left;
+		margin-top: 72px;
+		border-top-width: 0px;
+		background-color:  #707b7c  ;
+	}
+	.description{
+		font-family: oswald-medium;
+		font-size: 35px;
+		margin-bottom: 0px;
+		margin-top: 90px;
+	}
+	.content{
+		font-family: roboto-light;
+		font-size: 14px;
+	}
+	.links{
+		width: 950px;
+		height: 75px;
+		margin-left: auto;
+		margin-right: auto;
+		background-color: #2ecc71 ;
+		text-align: center;
+	}
+	.circle{
+		height: 45px;
+		width: 45px;
+		border-radius: 50%;
+		background-color:  #2ecc71 ;
+		display: inline-block;
+		margin-top: 13px;
+		margin-right: 35px;
+		vertical-align: top;
+	}
+	span.circle:hover{
+		background-color:  #28b463;
+	}
+	.fa-facebook-f, .fa-twitter, .fa-instagram, .fa-google, .fa-linkedin, .fa-github{
+		color: white;
+		margin-top: 10px;
+		font-size: 25px;
+	}
+</style>
+</head>
+<body>
+	<div class="profile">
+		<img src="profile.jpg" class="image" />
+		<h1 class="name">Jasffer Padigdig</h1>
+		<h2 class="section">Section: BSCS SS-191</h2><h2 class="contact">Contact: jtpadigdig@student.apc.edu.ph/09399176034</h2>
+		<hr class="line">
+		<h2 class="description">Description:</h2>
+		<h3 class="content">
+			Hello I'm Jasffer T. Padigdig and i'm from Quezon city.
+			I don't have any ideas regarding user design however, i think i'm going to enjoy and learn alot from this class.
+			I had this trello account when it was my first year in senior high and the only time i really got to use this account was when it was first introduced to us and now, i never really got a chance to use this account that much. I like reading, m favorite book right now is the city and the stars by arthur clarke, i also like watching any shows or dramas as long as it's interesting for me, as of now i just finished watching a k-drama series IOTNBO
+		</h3>
+	</div>
+	<div class="links">
+		<a href="https://www.facebook.com/JasfferTP/"><span class="circle"><i class="fab fa-facebook-f"></i></span></a>
+		<a href="https://twitter.com/JasfferTP"><span class="circle"><i class="fab fa-twitter"></i></span></a>
+		<a href="https://www.instagram.com/jasffertp/?hl=en"><span class="circle"><i class="fab fa-instagram"></i></span></a>
+		<a href="mailto: jasfferp@gmail.com"><span class="circle"><i class="fab fa-google"></i></span></a>
+		<a href="https://www.linkedin.com/in/jasffer-padigdig-043a63192/"><span class="circle"><i class="fab fa-linkedin"></i></span></a>
+		<a href="https://github.com/Jasffertp"><span class="circle"><i class="fab fa-github"></i></span></a>
+	</div>
+</body>
+</html>
